641150a14d1c8cd0f7d441d6289dbf16ffac0a47
Teddy Tennant <teddytennant@icloud.com>
2026-03-28 21:37:01 -0400

    Export all net modules: behaviour, events, node, protocol, topics

--- crates/nous-net/src/lib.rs
+++ crates/nous-net/src/lib.rs
@@ -1,5 +1,10 @@
+pub mod behaviour;
+pub mod events;
 pub mod node;
 pub mod protocol;
+pub mod topics;
 
-pub use node::NodeConfig;
+pub use events::{NodeEvent, WireMessage};
+pub use node::{NodeConfig, NousNode};
 pub use protocol::NousProtocol;
+pub use topics::NousTopic;
