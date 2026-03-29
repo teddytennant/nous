98717aa0b62c360202d4c2aa2fcb555346aa555f
Teddy Tennant <teddytennant@icloud.com>
2026-03-29 01:50:37 -0400

    Add NixOS shell.nix with GTK/WebKit dependencies for Tauri builds

---
+++ apps/desktop/shell.nix
@@ -0,0 +1,16 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+    pkg-config
+    glib
+    gdk-pixbuf
+    gtk3
+    webkitgtk_4_1
+    libsoup_3
+    pango
+    cairo
+    atk
+    harfbuzz
+  ];
+}
