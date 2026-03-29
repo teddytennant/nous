ece2a6d959f1011e4c39fda31bb1d999bedeedd5
Teddy Tennant <teddytennant@icloud.com>
2026-03-29 07:42:56 -0400

    Re-export FileCommand, MessageCommand, and VaultCommand from CLI lib

--- crates/nous-cli/src/lib.rs
+++ crates/nous-cli/src/lib.rs
@@ -2,6 +2,9 @@ pub mod commands;
 pub mod config;
 pub mod output;
 
-pub use commands::{Cli, Command, IdentityCommand, NetCommand, SocialCommand, WalletCommand};
+pub use commands::{
+    Cli, Command, FileCommand, IdentityCommand, MessageCommand, NetCommand, SocialCommand,
+    VaultCommand, WalletCommand,
+};
 pub use config::CliConfig;
 pub use output::Output;
