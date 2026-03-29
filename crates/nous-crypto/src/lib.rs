a73105acfdd3669a803fd5dbe36b6c862890aefb
Teddy Tennant <teddytennant@icloud.com>
2026-03-28 22:27:40 -0400

    Export zkp module with Schnorr proofs and Pedersen commitments

--- crates/nous-crypto/src/lib.rs
+++ crates/nous-crypto/src/lib.rs
@@ -1,7 +1,9 @@
 pub mod encryption;
 pub mod keys;
 pub mod signing;
+pub mod zkp;
 
 pub use encryption::{EncryptedPayload, decrypt, encrypt};
 pub use keys::{KeyPair, SharedSecret};
 pub use signing::{Signature, Signer, Verifier};
+pub use zkp::{PedersenCommitment, PedersenOpening, SchnorrProof};
