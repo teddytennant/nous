7c104c93444b54be0d559fee1acead5e391c5b83
Teddy Tennant <teddytennant@icloud.com>
2026-03-29 13:20:22 -0400

    Export MarketplaceCommand from CLI lib

--- crates/nous-cli/src/lib.rs
+++ crates/nous-cli/src/lib.rs
@@ -3,8 +3,8 @@ pub mod config;
 pub mod output;
 
 pub use commands::{
-    Cli, Command, FileCommand, IdentityCommand, MessageCommand, NetCommand, SocialCommand,
-    VaultCommand, WalletCommand,
+    Cli, Command, FileCommand, IdentityCommand, MarketplaceCommand, MessageCommand, NetCommand,
+    SocialCommand, VaultCommand, WalletCommand,
 };
 pub use config::CliConfig;
 pub use output::Output;
