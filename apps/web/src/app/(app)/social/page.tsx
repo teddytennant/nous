1a8ec11867c79f3a560e3a0936306fbdf75572bc
Teddy Tennant <teddytennant@icloud.com>
2026-03-31 05:01:30 -0400

    Add toast to useCallback deps in social page

--- apps/web/src/app/(app)/social/page.tsx
+++ apps/web/src/app/(app)/social/page.tsx
@@ -34,7 +34,7 @@ export default function SocialPage() {
     } finally {
       setLoading(false);
     }
-  }, []);
+  }, [toast]);
 
   usePageShortcuts({
     n: () => document.querySelector<HTMLTextAreaElement>("textarea")?.focus(),
